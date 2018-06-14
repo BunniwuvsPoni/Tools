7342905fd9a58d0756fac4c95c22280f1ba5dc22
Bunni <zichutonywang@gmail.com>
2018-06-14 10:57:41 -0400

    Add files via upload

---
+++ Update SharePoint Profiles from AD.ps1
@@ -0,0 +1,35 @@
+﻿Add-PSSnapin Microsoft.Sharepoint.Powershell
+
+# Note: You may have to update permissions in SharePoint Central Administrations for the update to be accepted, this script runs as an Admin of the SharePoint environment from the Sharepoint Server.
+
+# Log file path
+$desktoppath = [Environment]::GetFolderPath("Desktop") + "\SharePoint Sync Log.txt"
+
+foreach ($url in $webapp.sites.url){
+    
+    date | Tee-Object -FilePath $desktoppath -Append
+
+    echo $url | Tee-Object -FilePath $desktoppath -Append
+
+    # Display the existing Email
+    $users = Get-SPUser -Web $url
+    $users | Select UserLogin,DisplayName,Email | Tee-Object -FilePath $desktoppath -Append
+
+
+    foreach ($user in $users) {
+
+        echo $user.UserLogin | Tee-Object -FilePath $desktoppath -Append
+
+        if($user.UserLogin.StartsWith("i")) {
+        
+            # Syncs the user profile with Active Directory if the username begins with an "i"
+            Set-SPUser -Identity $user.UserLogin -Web $url -SyncFromAD
+
+        }
+
+    }
+
+    # Display updated email
+    $users = Get-SPUser -Web $url
+    $users | Select UserLogin,DisplayName,Email | Tee-Object -FilePath $desktoppath -Append
+}
---
+++ Windows Update Detect.ps1
@@ -0,0 +1,3 @@
+﻿#This script forces a Windows Update scan, this is useful if you are unable to check for Windows Updates due to updates being installed etc.
+$AutoUpdates = New-Object -ComObject "Microsoft.Update.AutoUpdate"
+$AutoUpdates.DetectNow()

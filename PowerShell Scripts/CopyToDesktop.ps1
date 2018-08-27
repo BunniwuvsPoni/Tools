379428c17cfb49dc8a00d0a81f2731d855f9a1d8
Bunni <zichutonywang@gmail.com>
2018-08-27 09:48:39 -0400

    Add files via upload

---
+++ PowerShell Scripts/CopyToDesktop.ps1
@@ -0,0 +1,7 @@
+﻿$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$File = $DesktopPath + "\(File name)"
+
+if(![System.IO.File]::Exists($File)){
+    # file with path $path doesn't exist
+    xcopy "Src" "Dst"
+}

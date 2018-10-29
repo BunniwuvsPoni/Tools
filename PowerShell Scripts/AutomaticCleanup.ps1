7098f53c158b4b2a91cfded6e888b3dc11c34275
Bunni <zichutonywang@gmail.com>
2018-10-29 16:41:36 -0400

    Update AutomaticCleanup.ps1

--- PowerShell Scripts/AutomaticCleanup.ps1
+++ PowerShell Scripts/AutomaticCleanup.ps1
@@ -1,7 +1,7 @@
 ### This powershell script is designed to clean out the "<File path to folder>" directory based on "<negative # of days>"
 ## i.e. Replace "File path to folder" with "C:\Users\<Username>\Downloads\" and "negative # of days" with -7 to remove downloads older than 7 days
 ## Get-ChildItem –Path "C:\Users\<Username>\Downloads\" -Recurse | Where-Object {!$_.PSIsContainer} | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-7))} | Remove-Item
-## powershell.exe -ExecutionPolicy Bypass "File path to powershell script".ps1
+## Run this script with: powershell.exe -ExecutionPolicy Bypass "File path to powershell script".ps1
 
 # Log file path
 $desktoppath = [Environment]::GetFolderPath("Desktop") + "\Automatic Cleanup Log.txt"

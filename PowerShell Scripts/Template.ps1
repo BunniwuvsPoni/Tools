4bf2217bf882ab023bac96daa42ba61e0d9e9e69
Bunni <zichutonywang@gmail.com>
2018-11-15 12:57:19 -0500

    Create Template.ps1

---
+++ PowerShell Scripts/Template.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER <param>
+
+.EXAMPLE
+
+.LINK
+
+#>
+
+# Required parameters to process O365 permissions change
+param(
+    [Parameter(Mandatory=$true)][string]$<param>
+    )

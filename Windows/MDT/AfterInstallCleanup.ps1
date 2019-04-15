96d7e6e1f1785007df47ea8984def3c2a1a5f705
Bunni <zichutonywang@gmail.com>
2019-04-15 14:58:14 -0400

    Update AfterInstallCleanup.ps1

--- Windows/MDT/AfterInstallCleanup.ps1
+++ Windows/MDT/AfterInstallCleanup.ps1
@@ -1,7 +1,11 @@
-﻿# Post MDT installation cleanup
+# Post MDT installation cleanup
+# Run with powershell.exe -executionpolicy bypass \\<path>\AfterInstallCleanup.ps1
 
 # This step disables the local Administrator account
 net user Administrator /active:no
 
 # This step enables BitLocker on the C: drive
 Enable-BitLocker -MountPoint “C:” -EncryptionMethod Aes128 –UsedSpaceOnly –RecoveryPasswordProtector
+
+# Reboot the computer
+Restart-Computer

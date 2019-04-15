2bb90efaa5a2c015bd63098043dbc75e746076f5
Bunni <zichutonywang@gmail.com>
2019-04-15 10:05:02 -0400

    Add files via upload

---
+++ Windows/AfterInstallCleanup.ps1
@@ -0,0 +1,7 @@
+﻿# Post MDT installation cleanup
+
+# This step disables the local Administrator account
+net user Administrator /active:no
+
+# This step enables BitLocker on the C: drive
+Enable-BitLocker -MountPoint “C:” -EncryptionMethod Aes128 –UsedSpaceOnly –RecoveryPasswordProtector

53f7a49a6f8209179330b81f680c5889a34a9e18
Bunni <zichutonywang@gmail.com>
2019-04-16 09:01:40 -0400

    Update AfterInstallCleanup.ps1

--- Windows/MDT/AfterInstallCleanup.ps1
+++ Windows/MDT/AfterInstallCleanup.ps1
@@ -6,8 +6,8 @@ net user Administrator /active:no
 
 # This step activates Windows with the original OEM key
 $Productkey = (Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductkey # Get the original OEM key
-cscript /b C:\Windows\System32\slmgr.vbs -ipk $Productkey # Install the OEM key
-cscript /b C:\Windows\System32\slmgr.vbs -ato # Activate the OEM key with Microsoft online
+slmgr.vbs -ipk $Productkey # Install the OEM key
+slmgr.vbs -ato # Activate the OEM key with Microsoft online
 
 # This step enables BitLocker on the C: drive
 Enable-BitLocker -MountPoint “C:” -EncryptionMethod Aes128 –UsedSpaceOnly –RecoveryPasswordProtector

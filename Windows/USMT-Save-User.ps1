77a9b976e3b00b585cf7a69e68215321ffcb94cd
Bunni <zichutonywang@gmail.com>
2019-04-17 13:55:13 -0400

    Add files via upload
    Added this WIP, not planning to finish this currently as the migration computers have a non standard configuration where the userprofiles may not be where it is normally expected.

---
+++ Windows/USMT-Save-User.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+This script uses USMT to save the specified user to the specified network share.
+.DESCRIPTION
+This script automates the steps behind saving a specific user profile using USMT.
+.PARAMETER Username
+.EXAMPLE
+& '.\USMT-Save-User.ps1' -Username "username"
+.LINK
+https://docs.microsoft.com/en-us/windows/deployment/usmt/usmt-scanstate-syntax
+#>
+
+# Required parameters to process O365 permissions change
+param(
+    [Parameter(Mandatory=$true)][string]$Username
+    )
+
+# Tests if user profile exists on the C: drive
+Test-Path "C:\Users\" $Username
+
+# Fomat current datetime
+$Date = Get-Date
+$FormattedDate = $Date.ToString("yyyy-mm-dd-hh-mm-ss")
+

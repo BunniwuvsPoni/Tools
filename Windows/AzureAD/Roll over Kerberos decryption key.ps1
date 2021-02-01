9fbf7935e7ecd5c03e8e6afb34d4b01e9a63b0f0
Bunni <zichutonywang@gmail.com>
2021-02-01 14:29:29 -0500

    Create Roll over Kerberos decryption key.ps1

---
+++ Windows/AzureAD/Roll over Kerberos decryption key.ps1
@@ -0,0 +1,16 @@
+# Must be ran in administrative PowerShell
+# Must have Microsoft 365 Global Administrator account
+# Must have Active Directory Domain Administrator account
+# Note: Ensure that you don't run the Update-AzureADSSOForest command more than once. Otherwise, the feature stops working until the time your users' Kerberos tickets expire and are reissued by your on-premises Active Directory.
+
+# Naviage to Azure AD Connect directory and import AzureADSSO PowerShell
+cd "C:\Program Files\Microsoft Azure Active Directory Connect"
+Import-Module .\AzureADSSO.psd1
+
+# Get list of AD forests with SSO enabled
+New-AzureADSSOAuthenticationContext
+Get-AzureADSSOStatus | ConvertFrom-Json
+
+# Roll over Kereberos decryption key
+$creds = Get-Credential
+Update-AzureADSSOForest -OnPremCredentials $creds

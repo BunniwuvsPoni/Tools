2962157e2caebd9f4d0b30bd686f2a0d87a9462c
Bunni <zichutonywang@gmail.com>
2021-02-02 09:45:26 -0500

    Update Prevent screen lock.ps1

--- Windows/PowerShell Scripts/Prevent screen lock.ps1
+++ Windows/PowerShell Scripts/Prevent screen lock.ps1
@@ -1,11 +1,16 @@
 # Prevents screen lock during the specified start and end times
 
+# Loading assembly...
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+# Configuration
 $start = Get-Date '07:00'
 $end = Get-Date '17:00'
+$frequency = 30
 
 while ($true) {
-    # Checks every 30 seconds
-    Start-Sleep -Seconds 30
+    # Checks every (x) seconds
+    Start-Sleep -Seconds $frequency
     
     # Gets the current date and time
     $date = Get-Date
@@ -16,6 +21,7 @@ while ($true) {
         # Moves the mouse one pixel to the right and back
         $Pos = [System.Windows.Forms.Cursor]::Position
         [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 1) , $Pos.Y)
+        $Pos = [System.Windows.Forms.Cursor]::Position
         [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) - 1) , $Pos.Y)
 
         # Logging to PowerShell

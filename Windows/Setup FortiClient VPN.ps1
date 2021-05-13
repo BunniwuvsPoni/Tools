508b00e9b9bd1b0cc814db0701932d1c0386bb30
Bunni <zichutonywang@gmail.com>
2021-05-13 16:01:38 -0400

    Create Setup FortiClient VPN.ps1

---
+++ Windows/Setup FortiClient VPN.ps1
@@ -0,0 +1,4 @@
+$Name = '<VPN Name>'
+$ServerAddress = '<VPN URL>'
+$AppID = 'FortinetInc.FortiClient_sq9g7krz3c65j'
+Add-VPNConnection -Name $Name -ServerAddress $ServerAddress -PlugInApplicationID $AppID -CustomConfiguration '<forticlient>0</forticlient>'

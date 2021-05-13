e3f9f11124ff37973f7bf13e5aa12bf89d032bd4
Bunni <zichutonywang@gmail.com>
2021-05-13 16:13:42 -0400

    Update AfterInstallCleanup.ps1

--- Windows/MDT/AfterInstallCleanup.ps1
+++ Windows/MDT/AfterInstallCleanup.ps1
@@ -7,5 +7,11 @@ net user Administrator /active:no
 # This step enables BitLocker on the C: drive
 Enable-BitLocker -MountPoint “C:” -EncryptionMethod Aes128 –UsedSpaceOnly –RecoveryPasswordProtector
 
+# This step sets up the FortiClient VPN
+$Name = '<VPN Name>'
+$ServerAddress = '<VPN URL>'
+$AppID = 'FortinetInc.FortiClient_sq9g7krz3c65j'
+Add-VPNConnection -Name $Name -ServerAddress $ServerAddress -PlugInApplicationID $AppID -CustomConfiguration '<forticlient>0</forticlient>'
+
 # Reboot the computer
 Restart-Computer -Force

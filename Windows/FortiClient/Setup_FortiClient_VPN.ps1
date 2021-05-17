748c5155facc605e6bc97b89921710b38c1e1b5f
Bunni <zichutonywang@gmail.com>
2021-05-17 15:22:04 -0400

    Update Setup_FortiClient_VPN.ps1

--- Windows/FortiClient/Setup_FortiClient_VPN.ps1
+++ Windows/FortiClient/Setup_FortiClient_VPN.ps1
@@ -3,4 +3,4 @@
 $Name = '<VPN Name>'
 $ServerAddress = '<VPN URL>'
 $AppID = 'FortinetInc.FortiClient_sq9g7krz3c65j'
-Add-VPNConnection -Name $Name -ServerAddress $ServerAddress -PlugInApplicationID $AppID -CustomConfiguration '<forticlient>0</forticlient>'
+Add-VPNConnection -Name $Name -ServerAddress $ServerAddress -PlugInApplicationID $AppID -CustomConfiguration '<forticlient>0</forticlient>' -SplitTunneling

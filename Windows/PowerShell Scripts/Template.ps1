29fd2b7ce6429e707c51cfe0c523706e04c0bb35
Bunni <zichutonywang@gmail.com>
2021-09-23 10:16:14 -0400

    Update Template.ps1

--- Windows/PowerShell Scripts/Template.ps1
+++ Windows/PowerShell Scripts/Template.ps1
@@ -15,3 +15,13 @@
 param(
     [Parameter(Mandatory=$true)][string]$<param>
     )
+
+echo "Please confirm the [condition] the script should terminate (y/n):"
+
+$readhost = Read-Host "(y / n)"
+
+if($readhost -eq 'n'){
+    echo "User did not confirm the specified [condition], closing function."
+    Read-Host -Prompt "Press Enter to exit"
+    Exit
+}

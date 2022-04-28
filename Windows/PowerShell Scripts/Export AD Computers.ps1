a499b1ef20ea6726af52fe02ecf6d66b0336f39f
Bunni <zichutonywang@gmail.com>
2022-04-28 07:58:08 -0400

    Update Export AD Computers.ps1

--- Windows/PowerShell Scripts/Export AD Computers.ps1
+++ Windows/PowerShell Scripts/Export AD Computers.ps1
@@ -5,4 +5,4 @@ $path = (file path)
 $path = $path + "Computers.csv"
 
 # Get computes and export to path
-Get-ADComputer -Filter * -Property * | Select-Object Name,OperatingSystem,OperatingSystemVersion,ipv4Address | Export-CSV $path-NoTypeInformation -Encoding UTF8
+Get-ADComputer -Filter * -Property * | Select-Object Name,OperatingSystem,OperatingSystemVersion,ipv4Address | Export-CSV $path -NoTypeInformation -Encoding UTF8

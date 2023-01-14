e2b82a71925169440321b6cfcc0d139cca611a1d
Bunni <zichutonywang@gmail.com>
2023-01-14 16:42:32 -0500

    Update Template.ps1

--- Windows/PowerShell Scripts/Template.ps1
+++ Windows/PowerShell Scripts/Template.ps1
@@ -16,12 +16,31 @@ param(
     [Parameter(Mandatory=$true)][string]$<param>
     )
 
+###
+
 echo "Please confirm the [condition] the script should terminate (y/n):"
 
 $readhost = Read-Host "(y / n)"
+
+###
 
 if($readhost -eq 'n'){
     echo "User did not confirm the specified [condition], closing function."
     Read-Host -Prompt "Press Enter to exit"
     Exit
+}
+
+###
+
+# User selects the folder where the PDFs are located
+Add-Type -AssemblyName System.Windows.Forms
+$browser = New-Object System.Windows.Forms.FolderBrowserDialog
+$null = $browser.ShowDialog()
+$folders = $browser.SelectedPath
+
+# Validates that a folder was selected
+if ([string]::IsNullOrEmpty($folders)) {
+    echo "No folder was selected, exiting script..."
+    Read-Host -Prompt “Press ENTER to continue...”
+    exit
 }

09cff4067819eb3528c1ed3f76a47d319881a5e0
Bunni <zichutonywang@gmail.com>
2023-02-01 16:45:50 -0500

    Update EXO - Enable Archiving and Litigation Holds.ps1

--- O365/Exchange Online/EXO - Enable Archiving and Litigation Holds.ps1
+++ O365/Exchange Online/EXO - Enable Archiving and Litigation Holds.ps1
@@ -1,7 +1,20 @@
 # This powershell script is intended to set all user type mailboxes in Exchange Online to enable 1) Archiving and 2) Litigation Hold
-# Please note, you need to install Exchange Online Powershell V2 for this to work
+# Please note, you need to install Exchange Online Powershell for this to work
 
-# Connect to Exchange Online
+# Connecting to Microsoft 365 Exchange Online PowerShell
+# Find the local installation of Exchange Online PowerShell Module
+$exchangeonlinepowershellinstalled = Get-InstalledModule -Name "ExchangeOnlineManagement"
+
+# Check if Exchange Online Powershell Module exists
+if ($exchangeonlinepowershellinstalled -eq $null) {
+    Write-Host "Exchange Online PowerShell Module is not installed, exiting script." -ForegroundColor Red
+    Exit
+}
+else {
+    Write-Host "Exchange Online PowerShell Module is installed, proceeding with script." -ForegroundColor Green
+}
+
+# Create the session (need to provide administrator username and password)
 Connect-ExchangeOnline
 
 # Enable Archiving
@@ -11,4 +24,4 @@ Get-Mailbox -Filter {RecipientTypeDetails -eq "UserMailbox"} | Enable-Mailbox -A
 Get-Mailbox -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-Mailbox -LitigationHoldEnabled $true
 
 # Display the results
-Get-Mailbox -Filter {RecipientTypeDetails -eq "UserMailbox"} | ft Name, Alias, PrimarySmtpAddress, ArchiveStatus, LitigationHoldEnabled
+Get-Mailbox -Filter {RecipientTypeDetails -eq "UserMailbox"} | Sort-Object PrimarySmtpAddress | ft Name, Alias, PrimarySmtpAddress, ArchiveStatus, LitigationHoldEnabled

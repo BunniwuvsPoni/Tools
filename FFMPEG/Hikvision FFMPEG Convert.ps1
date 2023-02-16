38c8943a9e0b2e68ea2c7287e4bcc3dbeeb1d7f2
Bunni <zichutonywang@gmail.com>
2023-02-16 08:54:44 -0500

    Update Hikvision FFMPEG Convert.ps1

--- FFMPEG/Hikvision FFMPEG Convert.ps1
+++ FFMPEG/Hikvision FFMPEG Convert.ps1
@@ -1,13 +1,14 @@
 # This script is intended to take a working directory, locate all .mp4 files within that directory, and convert the .mp4 files using FFMPEG outputted into a Converted folder within said working directory
 # This is useful for Hikvision .mp4 files that use the H.265 video and G711U audio codec and convert it into a .mp4 file that can be viewed on most media players
 
+# Dependencies
+# FFmpeg: https://ffmpeg.org/
+# -> Must be installed/extracted into the ("C:\Program Files\FFMPEG\bin") directory
+# -> Must be added to the Systems PATH Environmental Variables (Settings -> About -> Advanced system settings -> Environment Variables -> System variables -> Path -> Edit -> New)
+
 # Obtain the working directory
 $application = New-Object -ComObject Shell.Application
 $workingDirectory = ($application.BrowseForFolder(0, 'Select a folder', 0)).Self.Path
-
-# FFMPEG directory, ffmpeg.exe must be in the specified directory
-$ffmpeg = "C:\Program Files\FFMPEG\bin"
-cd $ffmpeg
 
 # Log file
 $log = "\Log.txt"
@@ -44,7 +45,7 @@ foreach($file in $filesToConvert) {
     Write-Output "Converting file:" $file.Name | Tee-Object -FilePath $logUpdated -Append
     $fileConvert = $workingDirectory + "\" + $file.Name
     $fileConverted = $convertedDirectory + "\" + $file.Name
-    .\ffmpeg.exe -i $fileConvert $fileConverted
+    ffmpeg.exe -i $fileConvert $fileConverted
     Write-Output "File converted:" $file.Name | Tee-Object -FilePath $logUpdated -Append
 }
 

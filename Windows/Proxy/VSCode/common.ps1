9a4400ef0bae92d404639e1c0e62c34d07e68b90
Bunni <zichutonywang@gmail.com>
2024-08-08 15:05:18 -0400

    Create common.ps1

---
+++ Windows/Proxy/VSCode/common.ps1
@@ -0,0 +1,15 @@
+## Get the password
+$message="Enter Password for " + $env:username
+$password = Read-Host -Prompt $message -AsSecureString
+$MYCPW=[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+# Create proxy string
+$proxy='http://'+ $env:username + ':' + $MYCPW +'@proxy.domain.com:8080'
+
+# Set environmental variable with proxy
+$env:HTTPS_PROXY=$proxy
+$env:HTTP_PROXY=$proxy
+
+# Set .gitconfig proxy
+git config --global https.proxy $proxy
+git config --global http.proxy $proxy
